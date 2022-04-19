0dcb7b0920bcc71907dab65da4efc961a9ed910b
Jeanette <melange@Melinas-Air.hsd1.ma.comcast.net>
2022-04-19 14:53:21 -0400

    seed data added

---
+++ src/data.js
@@ -0,0 +1,18 @@
+const toDoData = [
+    {
+        title: "Get out of Bed",
+        done: true,
+    },
+
+    {
+        title: "Breath",
+        done: true,
+    },
+    {
+        title: "Do better than yesterday",
+        done: false,
+    }
+
+];
+
+export default toDoData
